b2fdfc444d6a18d46be09c1f6bd630ebe0fd2acd
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-02 17:47:45 +0700

    transaksi manual

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/order-product-manual', function() {
+    // memulai transaksi
+    DB::beginTransaction();
+    try {
+    // Membuat record di table `orders`
+    $order_id = DB::table('orders')->insertGetId(['customer_id'=>1]);
+    // Menambah record baru di `order_products`
+    DB::table('orders_products')->insert(['order_id'=>$order_id, 'product_id'=>5]);
+    // throw new Exception("Koneksi ke database terputus..");
+    } catch (Exception $e) {
+    DB::commit();
+        return "Error: " . $e->getMessage();
+    }
+    try {
+    // Membayar order (mengisi field `paid_at` di table `orders`)
+    DB::table('orders')->where('id',$order_id)->update(['paid_at'=>new DateTime]);
+    // throw new Exception("Server Database mati..");
+    // Mengurangi stock product
+    DB::table('products')->decrement('stock');
+    } catch (Exception $e) {
+    DB::rollback();
+        return "Error: " . $e->getMessage();
+    }
+    // meyimpan transaksi
+    DB::commit();
+    echo "Berhasil menjual " . DB::table('products')->find(5)->name . '. <br>';
+    echo "Stock terkini : " . DB::table('products')->find(5)->stock;
+});
+
 Route::get('/order-product', function() {
     // memulai transaksi
     DB::transaction(function()
