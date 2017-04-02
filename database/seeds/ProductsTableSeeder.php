c90d3e6a76ee90dc33930cc3458cb9ea06fe0337
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-02 13:44:23 +0700

    tambah schema builder products

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -11,6 +11,20 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('products')->insert([
+         [
+           'name' => "Toyota Alphard",
+           'price' => 150000000,
+           'stock' => 4,
+         ],
+
+         [
+            'name' => "Toyota Avanza",
+            'price' => 350000,
+            'stock' => 8,
+         ]
+     ]);
+     
+     $this->command->info('Berhasil menambah 2 mobil!');
     }
 }
