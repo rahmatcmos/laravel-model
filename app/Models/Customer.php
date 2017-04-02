25b550c98f14ca79a9223004f53218f2c4412faa
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-02 20:18:55 +0700

    tambah referensi

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+      // public $timestamps = false;
+      // protected $primaryKey = 'product_id';
+      // protected $connection = 'mysql2';
 }
