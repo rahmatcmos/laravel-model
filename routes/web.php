70bc5042d767746f49660d89bcc1f272f192d6b6
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-02 17:34:51 +0700

    transaksi otomatis

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/order-product', function() {
+    // memulai transaksi
+    DB::transaction(function()
+    {
+    // Membuat record di table `orders`
+    $order_id = DB::table('orders')->insertGetId(['customer_id'=>1]);
+    // Menambah record baru di `order_products`
+    DB::table('orders_products')->insert(['order_id'=>$order_id, 'product_id'=>5]);
+    // Membayar order (mengisi field `paid_at` di table `orders`)
+    DB::table('orders')->where('id',$order_id)->update(['paid_at'=>new DateTime]);
+    // Terjadi error..
+    // throw new Exception("Oooppssss.. ada error!");
+    // Mengurangi stock product
+    DB::table('products')->decrement('stock');
+    });
+    echo "Berhasil menjual " . DB::table('products')->find(5)->name . '. <br>';
+    echo "Stock terkini : " . DB::table('products')->find(5)->stock;
+});
+
 Route::get('/list-stock', function() {
     $begin = memory_get_usage();
     foreach (DB::table('products')->get() as $product) {
