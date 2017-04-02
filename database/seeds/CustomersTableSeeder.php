624abe09a0427acc7f756d0b77d66ae0e907a25d
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-02 17:08:13 +0700

    tambah membershiptype

--- database/seeds/CustomersTableSeeder.php
+++ database/seeds/CustomersTableSeeder.php
@@ -13,12 +13,14 @@ class CustomersTableSeeder extends Seeder
     public function run()
     {
       $faker = Faker::create();
+      $membership_type_id = [null, 1, 2];
       foreach ( range(1,10) as $index ) {
           DB::table('customers')->insert([
              [
                'name' => $faker->name,
                'phone' => $faker->phoneNumber,
-               'address' => $faker->address
+               'address' => $faker->address,
+               'membership_type_id' => $membership_type_id[rand(0,2)]
              ]
           ]);
       }
