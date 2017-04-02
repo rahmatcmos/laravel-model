cf1e46e61aeddff4024f8e946c216f36ed2a6d1f
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-02 16:01:11 +0700

    sample chunk seed

--- database/seeds/SampleChunkSeeder.php
+++ database/seeds/SampleChunkSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
 class SampleChunkSeeder extends Seeder
@@ -11,6 +12,20 @@ class SampleChunkSeeder extends Seeder
      */
     public function run()
     {
-        //
+      $faker = Faker::create();
+      $products = ["Accord", "Civic", "City", "CR-V", "Jazz", "Freed", "Mobilio"];
+      $descriptions = ["Tipe Manual", "Tipe Otomatis"];
+      for ( $i=0; $i < 10000; $i++ ) {
+          DB::table('products')->insert([
+             [
+               'name' => $products[rand(0,6)] . ' ' . $faker->firstNameMale,
+               'description' => $descriptions[rand(0,1)],
+               'price' => rand(100,800) * 1000000,
+               'stock' => rand(2,40)
+             ]
+          ]);
+      }
+
+      $this->command->info('Berhasil 10.000 menambah mobil!');
     }
 }
