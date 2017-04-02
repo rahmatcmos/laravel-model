92813227df7fba415678fc767d9bcadded15d387
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-02 17:08:01 +0700

    buat seed membershiptype

--- database/seeds/MembershipTypesTableSeeder.php
+++ database/seeds/MembershipTypesTableSeeder.php
@@ -11,6 +11,16 @@ class MembershipTypesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = ["Silver", "Gold", "Platinum"];
+        $discounts = [5, 10, 15];
+        $fees = [100000, 300000, 600000];
+        foreach (range(0,2) as $index) {
+          DB::table('membership_types')->insert([
+            'type' => $types[$index],
+            'discount' => $discounts[$index],
+            'yearly_fee' => $fees[$index]
+          ]);
+        }
+        $this->command->info('Berhasil menambah membership_type_id!');
     }
 }
