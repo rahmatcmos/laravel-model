48919a0e6f726d84f863d415b018104893290139
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-02 20:39:44 +0700

    tambah referensi

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /* Timestamps
+    public $timestamps = false;
+    */
+
+    /* Hidden
+    protected $hidden = ['description', 'stock'];
+    */
+
+    /* Visible
+    protected $visible = ['name', 'price'];
+    */
 }
