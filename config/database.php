e2d83ae293e333b0161fe67fa715acee7b608269
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-02 19:41:56 +0700

    konfigurasi read/write

--- config/database.php
+++ config/database.php
@@ -40,6 +40,12 @@ return [
         ],
 
         'mysql' => [
+            'read' => [
+                'database' => env('DB_DATABASE', 'forge'),
+            ],
+            'write' => [
+                'database' => env('DB_DATABASE_2', 'laravel-db-model2'),
+            ],
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
