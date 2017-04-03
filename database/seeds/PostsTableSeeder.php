e7a54605438b92a2e7d6122d358a94b3aecba6ec
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-04 00:08:07 +0700

    tambah faker

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
 class PostsTableSeeder extends Seeder
@@ -11,6 +12,13 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+      $faker = Faker::create();
+      for ($i=0; $i <= 10 ; $i++) {
+        App\Post::insert([
+          'title' => $faker->sentence,
+          'content' => $faker->paragraph,
+          'published' => rand(0,1)
+        ]);
+      }
     }
 }
