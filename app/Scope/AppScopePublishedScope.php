465942c404302afe4af1bb0a955e4c09a892f52f
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-04 14:46:12 +0700

    tambah method apply

--- app/Scope/AppScopePublishedScope.php
+++ app/Scope/AppScopePublishedScope.php
@@ -2,8 +2,13 @@
 
 namespace App\Scope;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ScopeInterface;
 
 class PublishedScope implements ScopeInterface {
-
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where('published', 1);
+    }
 }
