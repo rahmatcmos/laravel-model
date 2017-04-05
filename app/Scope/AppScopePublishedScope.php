6128c1d1cf9ad9fbb5daa543a39e8636938906c7
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-05 14:30:25 +0700

    tambah remove

--- app/Scope/AppScopePublishedScope.php
+++ app/Scope/AppScopePublishedScope.php
@@ -11,4 +11,24 @@ class PublishedScope implements ScopeInterface {
     {
         $builder->where('published', 1);
     }
+
+    public function remove(Builder $builder, Model $model)
+    {
+        $query = $builder->getQuery();
+        foreach ((array) $query->wheres as $key => $where)
+        {
+            // mengecek apakah opsi where ini merupakan constraint untuk published
+            if ($where['type'] == 'Basic' && $where['column'] == 'published' && $where
+            ['value'] == 1) {
+            // menghapus query dari opsi where
+            unset($query->wheres[$key]);
+            $query->wheres = array_values($query->wheres);
+            // menghapus binding
+            $bindings = $query->getRawBindings()['where'];
+            unset($bindings[$key]);
+            $query->setBindings($bindings);
+          }
+        }
+        return $query;
+    }
 }
