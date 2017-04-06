78e2cef3b52525b38caa0b3cc2bdc1424aab02f4
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-06 21:46:11 +0700

    tambah viewed at

--- database/migrations/2017_04_03_235625_create_posts_table.php
+++ database/migrations/2017_04_03_235625_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->text('content');
             $table->boolean('published');
+            $table->timestamp('viewed_at');
             $table->timestamps();
         });
     }
