630cefed777371e42cf8aca62b6f6c55a40de840
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-06 21:46:18 +0700

    tambah getdates

--- app/Post.php
+++ app/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
     protected $connection = 'mysql2';
 
     protected $fillable = ['title', 'content', 'published'];
+
+    public function getDates()
+    {
+        return ['created_at', 'updated_at', 'viewed_at'];
+    }
 }
