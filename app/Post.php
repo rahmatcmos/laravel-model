af36292bff4be24005928fa0dc4e4bfd641beb4c
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-06 17:24:00 +0700

    add trait published

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use \App\Scope\PublishedTrait;
+    //use \App\Scope\PublishedTrait;
+    use \App\Scope\Published;
 
     protected $fillable = ['title', 'content', 'published'];
 }
