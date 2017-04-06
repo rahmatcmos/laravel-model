9d81173ab06daeb37d6b6c10eac281a4fa904f39
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-06 21:47:22 +0700

    tambah viewed at carbon

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -24,6 +24,7 @@ class PostsTableSeeder extends Seeder
         $post->title = $faker->sentence;
         $post->content = $faker->paragraph;
         $post->published = rand(0,1);
+        $post->viewed_at = Carbon\Carbon::now()->addDays(rand(1,5))->addHours(rand(0,18));
         $post->save();
       }
     }
