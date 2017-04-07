4586e9796cd9d79d1aa27a2306bb4339db8665da
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-07 07:01:48 +0700

    buat product log

--- app/ProductLog.php
+++ app/ProductLog.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductLog extends Model
 {
-    //
+    // Aktifkan mass assignment
+    protected $fillable = ['product_id', 'changes'];
+
+    // Menggunakan mutator
+    public function setChangesAttribute($value)
+    {
+        $this->attributes['changes'] = json_encode($value);
+    }
+
+    // Menggunakan accessor
+    public function getChangesAttribute($value)
+    {
+        return json_decode($value);
+    }
 }
